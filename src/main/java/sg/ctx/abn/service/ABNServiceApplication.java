aab836fd6bf8b42c87aeba6a49632af52e84cadf
yumiso <miao.yu@ctx.sg>
2021-12-21 16:26:48 +0800

    Initial commit

---
+++ src/main/java/sg/ctx/abn/service/ABNServiceApplication.java
@@ -0,0 +1,29 @@
+package sg.ctx.abn.service;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync //开启异步
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableScheduling
+@ComponentScan("sg.ctx")
+public class ABNServiceApplication implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ABNServiceApplication.class, args);
+    }
+}
